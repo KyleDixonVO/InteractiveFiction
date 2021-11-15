7278a54db50caa4ef82b9c3aaf52e96c235e6490
KyleDixonVO <KyleDixon@hotmail.ca>
2021-11-15 13:23:23 -0400

    Starting Out

--- InteractiveFiction/Program.cs
+++ InteractiveFiction/Program.cs
@@ -8,8 +8,35 @@ namespace InteractiveFiction
 {
     class Program
     {
+        static string[,] story = new string[64,64];
+        static char playerInput;
+        static int pageRoot;
+
+
+        static void PageList()
+        {
+            story[0,0] = "You wake on a damp bed of leaves. Your head pounds, how did you get here? To the north you see a satchel on the ground. To the south you see a rope leading over the edge of a cliff.";
+            story[0,1] = "page 1";
+            story[0,2] = "page 2";
+            story[0,3] = "page 3";
+        }
+
+        static void PlayerActions()
+        {
+            playerInput = Console.ReadKey();
+            if (playerInput == 'a')
+            {
+
+            }
+            
+            
+        }
+        
         static void Main(string[] args)
         {
+            PageList();
+            Console.WriteLine(story[0,0]);
+            PlayerActions();
         }
     }
 }
