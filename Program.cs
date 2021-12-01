ce37995ddb89445b63c78c0805d7adc98f14d98b
Kyle Dixon <90637263+KyleDixonVO@users.noreply.github.com>
2021-11-30 22:35:32 -0400

    Moved all files up a level. Story now reads from text file. Game now supports saving and loading from a text file.
    Need to implement a system for the system to locate the text files on a system besides my home pc. Need to add checks for story triggers.

--- Program.cs
+++ Program.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Text.RegularExpressions;
+using System.IO;
 
 namespace InteractiveFiction
 {
     class Program
     {
-        static string[] story = new string[64]; //stores story pages
+        static string[] story; //stores story pages
         static string[] sentences; //used to split story pages
         static char playerInput; //stores char of player input
         static int choiceA; //stores the page number of the first choice
@@ -18,42 +18,17 @@ namespace InteractiveFiction
         static char delimiter = ':'; //where story pages are split
         static string[] keyWords = new string[3]; //for coloring the text
         static string[] lines; //used to split sentences
-        static int page = 0; //defaults page number to zero
-        
+        static int page; //defaults page number to zero
+
         //Some story options will utilize inventory items/tracked gamestate events (to be implemented later).
         //I plan to make my story require multiple loops through the choices, where some options are only available after certain conditions are met.
 
 
-        static void PageList() //list of all story pages, will be moved to a file later.
-        {
-            story[0] = "Shotty Game Studios\nWhispers\n--------------------\n#A or B. Start Game.:1:1";
-            story[1] = "You wake on a damp bed of leaves. Your head pounds, how did you get here?\nTo the north you see a satchel on the ground.To the south you see a rope leading over the edge of a cliff.\n#A. Go North\n#B. Go South:2:3"; //the option to take the satchel will not be there after it has been used in a previous loop.
-            story[2] = "You walk over to the bag, it smells of potent rot,the leather is slimy.\nThe bag pulses slightly.\n#A. Take The Bag?\n#B. Leave The Bag? :4:5";
-            story[3] = "You walk to the edge of the cliff and grab the rope, you descend to the base of the cliff.\n#A or B. Descend.:8:8";
-            story[4] = "You pick up the bag, it is warm to the touch, the leather ripples under your fingertips. You go to open the bag, but are met with a roiling sense of unease. Maybe the bag should stay closed.\n#A or B. Go to cliff edge.:3:3";
-            story[5] = "You decide to leave the bag, it’s gross and you’re pretty sure it’s not yours. You turn to leave, and you begin to hear whispers behind you. /Don’t leave us./\n#A. Turn Around.\n#B. Run.:6:7";
-            story[6] = "You turn around to investigate the source of the whispers. You don’t see anyone. The whispers grow louder, your head starts to hurt… A familiar bag rests at your side.\n#A or B. Go to cliff edge.:3:3";
-            story[7] = "You run from the whispers, you run to the edge of the cliff and grab the rope. You descend in a hurry.\n#A or B. Descend.:8:8";
-            story[8] = "The forest is thick here, mist hangs close to the ground. In the distance you hear a low hum.\n#A. Go East.\n#B. Go West.:9:10";
-            story[9] = "You walk east, and eventually come upon a small pond, the mist makes it hard to see, but you can make out your reflection in the water.\n#A. Look Closer.\n#B. Walk Around The Bank.:11:12";
-            story[10] = "You walk west, the humming begins to grow louder, and louder, until your form resonates with it. You travel further west, you see an altar nestled amongst the roots and underbrush, upon it rests a basin of fluid. The Hum is intoxicating. You step up to the altar.\n#A. Drink. \n#B. Kneel.:15:16";
-            story[11] = "You peer closer to the water, trying to get a better look at your reflection. You get closer and closer to the water, and stop. The face does not stop, and the creature below pulls you into the murky embrace of the pond.::";
-            story[12] = "You walk around the bank of the pond, noticing scattered fragments of bone at your feet. You hear something in the water. You turn to see a warped lanky humanoid, it looks at you hungrily.\n#A. Throw the satchel.\n#B. Flee.:13:14"; //when the inventory is implemented, throwing the satchel will only be available if the player picked it up.
-            story[13] = "You reach for the satchel, it pulses faster now. Is it afraid? You throw the satchel, it hits the humanoid and lands at it's feet. The creature picks up the bag and takes a bite, your head throbs in pain.:63:63"; //this sends the player back to the start, to begin another loop. When the player returns they will find a an old photo.
-            story[14] = "You tear off into the forest, you can hear it crashing through the trees behind you. Eventually you think you've lost it. You hear the hum to your west. You can also make out a standing stone to your north.\n#A. Go West. \n#B. Go North. :10:17";
-            story[15] = "You drink the fluid, but this communion does not absolve your guilt. You start to remember, your head pounds.:1:1"; //This sends the player back to begin another loop
-            story[16] = "You kneel at the altar, head bowed... You have not atoned. The basin spills. Your guilt eats you alive.::"; //if the player has discovered the truth, this option will be replaced, the truth has not been implemented.
-            story[17] = "You walk towards the standing stone, it marks a simple grave, recently filled.\n#A. Read the headstone. \n#B. Mourn. :18:19"; //if the player has the photo from the pond, they will be able to mourn.
-            story[18] = "The stone is simply cut, there are no dates on the stone, just a name. Your name. Your vision blurs, your head burns with searing pain.:63:63"; //this sends the player back to begin another loop.
-            story[19] = "You look at the photo, and you're reminded of simpler times. You lay the photo on the headstone, the pain in your head eases slightly.:63:63"; //the player must mourn in order to discover the truth. //The photo will no longer be available in subsequent loops.
-            story[20] = "";
 
-
-            story[63] = "The truth whispers to you.\nA or B. Try again.:1:1";
-        }
         static void PlayerActions() //gets the key pressed by the user and performs a task if the input is valid.
         {
-
+            Console.WriteLine("");
+            Console.WriteLine("Awaiting choice. A/B: Story Options. C: Save Game. D: Load Game.");
             playerInput = Console.ReadKey(true).KeyChar;
 
             if (playerInput == 'a' || playerInput == 'A')
@@ -68,26 +43,26 @@ namespace InteractiveFiction
             }
             else if (playerInput == 'c' || playerInput == 'C')
             {
-                //Save Game
+                SaveGame();
             }
             else if (playerInput == 'd' || playerInput == 'D')
             {
-               //Load Game
+                LoadGame();
             }
             else
             {
                 Console.WriteLine("Invalid input, please choose from a valid input");
                 PlayerActions();
             }
-            
+
         }
-        
+
         static void GetPageNumbers() //splits story pages at the delimter, parses page numbers into ints to be used in the player input method.
-        {                           
+        {
             Console.WriteLine();
             Console.WriteLine("Page: " + (page));
             sentences = story[page].Split(delimiter); //splits the current story page at every colon and stores it in the sentences array
-            string testA = sentences[sentences.Length-2]; //stores the value of the second last sentence as a string to be parsed
+            string testA = sentences[sentences.Length - 2]; //stores the value of the second last sentence as a string to be parsed
             bool isIntA = int.TryParse(testA, out int hasIntA);
             if (isIntA == true)
             {
@@ -99,7 +74,7 @@ namespace InteractiveFiction
             }
             string testB = sentences[sentences.Length - 1]; //stores the value of the last sentence as a string to be parsed
             bool isIntB = int.TryParse(testB, out int hasIntB);
-            if(isIntB == true)
+            if (isIntB == true)
             {
                 choiceB = int.Parse(testB); //parses the last string into int
             }
@@ -111,7 +86,10 @@ namespace InteractiveFiction
 
         static void Scribe() //writes the appropriate story page, needs to be reworked to properly display color
         {
-            Console.WriteLine(sentences[0]);
+            for (int i = 0; i < (sentences.Length - 2); i++)
+            {
+                Console.WriteLine(sentences[i]);
+            }
         }
 
         static void ChangeTextColor() //method to color individual lines based on keywords (work in progress)
@@ -167,18 +145,83 @@ namespace InteractiveFiction
             Console.ResetColor();
             Console.ReadKey(true);
         }
-        static void Main(string[] args)
+        static void Main()
         {
-           MainMenu();
-           while(gameOver == false) //gameplay loop, gets page data, parses the page numbers from the data, writes the relevant text to the screen, then waits for player input.
-           {
-                PageList();
+            MainMenu();
+            while (gameOver == false) //gameplay loop, gets page data, parses the page numbers from the data, writes the relevant text to the screen, then waits for player input.
+            {
+                ReadStoryTxt();
                 GetPageNumbers();
                 //ChangeTextColor();
                 Scribe();
-                PlayerActions();  
-           }
+                PlayerActions();
+            }
             EndGame();
+        }
+
+        static void ReadStoryTxt()
+        {
+            story = File.ReadLines(@"C:\Users\Kyle\Desktop\GitHub\InteractiveFiction\story.txt").Skip(4).ToArray();
+        }
+
+        static void LoadGame()
+        {
+            Console.WriteLine("Load Game? Any unsaved progress will be lost. Y/N");
+            ConsoleKey input = Console.ReadKey(true).Key;
+            if (input == ConsoleKey.Y)
+            {
+
+                string[] savePoint = File.ReadAllLines(@"C:\Users\Kyle\Desktop\GitHub\InteractiveFiction\savegame.txt").ToArray();
+                bool containsInt = int.TryParse(savePoint[1], out int savedPage);
+                if (containsInt == true)
+                {
+                    page = int.Parse(savePoint[1]);
+                    Console.WriteLine("Loaded Game!");
+                    Console.ReadKey(true);
+                }
+                else
+                {
+                    Console.WriteLine("Cannot find page number in savegame.txt");
+                    Console.ReadKey(true);
+                    Main();
+                }
+            }
+            else if (input == ConsoleKey.N)
+            {
+                PlayerActions();
+            }
+            else
+            {
+                Console.WriteLine("Invalid input, input must be 'Y' or 'N'");
+                LoadGame();
+            } 
+        }
+
+        static void SaveGame()
+        {
+            Console.WriteLine("Save Game? This will overwrite current save. Y/N");
+            ConsoleKey input = Console.ReadKey(true).Key;
+            if (input == ConsoleKey.Y)
+            {
+                string SavePoint = @"C:\Users\Kyle\Desktop\GitHub\InteractiveFiction\savegame.txt";
+
+                
+                string[] SaveData = { "Whispers Save Data", Convert.ToString(page) };
+                File.WriteAllLines(SavePoint, SaveData);
+
+                
+                Console.WriteLine("Saved Game!");
+                Console.ReadKey(true);
+            }
+            else if (input == ConsoleKey.N)
+            {
+                PlayerActions();
+            }
+            else
+            {
+                Console.WriteLine("Invalid input, input must be 'Y' or 'N'");
+                SaveGame();
+            }  
         }
     }
 }
