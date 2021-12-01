65c471f7f228c663f381d1df257a8b1aca884847
Kyle Dixon <90637263+KyleDixonVO@users.noreply.github.com>
2021-12-01 12:53:16 -0400

    Main menu now has a cursor that can be moved with W and S, added newgame, load, and quit functionality to  main menu. Added quit to player input options. Added GetFilePath to determine location of game folder on disk. Moved ASCII to it's own method. Added an error check to readStoryText. Added comments. Changed Save, Load, and readstorytext to use dynamic path. Removed scribe, reworked ChangeTextColor into Textmanger to handle writing and color.

--- Program.cs
+++ Program.cs
@@ -5,20 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
+
 namespace InteractiveFiction
 {
     class Program
     {
         static string[] story; //stores story pages
         static string[] sentences; //used to split story pages
-        static char playerInput; //stores char of player input
+        static ConsoleKey playerInput; //stores char of player input
         static int choiceA; //stores the page number of the first choice
         static int choiceB; //stores the page number of the second choice
         static bool gameOver = false; //checks if the game should end
         static char delimiter = ':'; //where story pages are split
         static string[] keyWords = new string[3]; //for coloring the text
-        static string[] lines; //used to split sentences
         static int page; //defaults page number to zero
+        static int optionSelected = 0;
+        static bool waitingForEnter = false;
+        static string useablePath;
 
         //Some story options will utilize inventory items/tracked gamestate events (to be implemented later).
         //I plan to make my story require multiple loops through the choices, where some options are only available after certain conditions are met.
@@ -28,26 +31,30 @@ namespace InteractiveFiction
         static void PlayerActions() //gets the key pressed by the user and performs a task if the input is valid.
         {
             Console.WriteLine("");
-            Console.WriteLine("Awaiting choice. A/B: Story Options. C: Save Game. D: Load Game.");
-            playerInput = Console.ReadKey(true).KeyChar;
+            Console.WriteLine("Awaiting choice. A/B: Story Options. C: Save Game. D: Load Game. Esc: Exit Game.");
+            playerInput = Console.ReadKey(true).Key;
 
-            if (playerInput == 'a' || playerInput == 'A')
+            if (playerInput == ConsoleKey.A)
             {
                 page = choiceA;
                 Console.Beep(625, 100);
             }
-            else if (playerInput == 'b' || playerInput == 'B')
+            else if (playerInput == ConsoleKey.B)
             {
                 page = choiceB;
                 Console.Beep(525, 100);
             }
-            else if (playerInput == 'c' || playerInput == 'C')
+            else if (playerInput == ConsoleKey.C)
             {
                 SaveGame();
             }
-            else if (playerInput == 'd' || playerInput == 'D')
+            else if (playerInput == ConsoleKey.D)
             {
                 LoadGame();
+            }
+            else if (playerInput == ConsoleKey.Escape)
+            {
+                gameOver = true;
             }
             else
             {
@@ -56,7 +63,6 @@ namespace InteractiveFiction
             }
 
         }
-
         static void GetPageNumbers() //splits story pages at the delimter, parses page numbers into ints to be used in the player input method.
         {
             Console.WriteLine();
@@ -83,44 +89,135 @@ namespace InteractiveFiction
                 gameOver = true;
             }
         }
-
-        static void Scribe() //writes the appropriate story page, needs to be reworked to properly display color
+        static void TextManager() //Checks sentences against a set of keywords to determine if they should receive color, then writes sentences.
         {
-            for (int i = 0; i < (sentences.Length - 2); i++)
-            {
-                Console.WriteLine(sentences[i]);
-            }
-        }
-
-        static void ChangeTextColor() //method to color individual lines based on keywords (work in progress)
-        {
-            lines = sentences[0].Split('\n'); //splitting the sentence at each new line
             keyWords[0] = "#A";
             keyWords[1] = "#B";
             keyWords[2] = "#A or B";
-            foreach (string line in lines) //checking each line in the array lines[]
+            for (int i = 0; i < sentences.Length-2; i++) //checking each line in the array lines[]
             {
-                string scraper = line;
                 foreach (string keyWord in keyWords) //comparing the line against each keyword in keywords
                 {
-                    bool containsKeyword = scraper.Contains(keyWord); //the check
+                    bool containsKeyword = sentences[i].Contains(keyWord); //the check
                     if (containsKeyword == true)
                     {
                         Console.ForegroundColor = ConsoleColor.Green; //sets text color to green
+                        break;
                     }
                     else
                     {
                         Console.ResetColor();
                     }
                 }
-                Console.WriteLine(line);
+                Console.WriteLine(sentences[i]);
+                Console.ResetColor();
             }
         }
-
-        static void MainMenu() //simple main menu, want to add load and save functionality later
+        static void MainMenu() //Splash screen with simple cursor input.
         {
             Console.BackgroundColor = ConsoleColor.DarkMagenta;
             Console.Clear();
+            TitleText();
+
+            Console.WriteLine("     New Game");
+            Console.WriteLine("     Load Game");
+            Console.WriteLine("     Quit Game");
+            Console.SetCursorPosition(2, 11);
+            Console.Write("->");
+
+
+            while (waitingForEnter == false)
+            {
+                Console.CursorVisible = false;
+                ConsoleKey moveCursor = Console.ReadKey(true).Key;
+                if (moveCursor == ConsoleKey.W)
+                {
+                    if (optionSelected > 0)
+                    {
+                        Console.Beep(625, 100);
+                        optionSelected--;
+                    }
+                }
+                else if (moveCursor == ConsoleKey.S)
+                {
+                    if (optionSelected < 2)
+                    {
+                        Console.Beep(525, 100);
+                        optionSelected++;
+                    }
+                }
+
+                if (optionSelected == 0)
+                {
+                    Console.SetCursorPosition(2, 12);
+                    Console.Write("  ");
+                    Console.SetCursorPosition(2, 11);
+                    Console.Write("->");
+                }
+                else if (optionSelected == 1)
+                {
+                    Console.SetCursorPosition(2, 11);
+                    Console.Write("  ");
+                    Console.SetCursorPosition(2, 13);
+                    Console.Write("  ");
+                    Console.SetCursorPosition(2, 12);
+                    Console.Write("->");
+                }
+                else if (optionSelected == 2)
+                {
+                    Console.SetCursorPosition(2, 12);
+                    Console.Write("  ");
+                    Console.SetCursorPosition(2, 13);
+                    Console.Write("->");
+                }
+
+                ConsoleKey enterPress = moveCursor;
+                if(enterPress == ConsoleKey.Enter)
+                { 
+                    if (optionSelected == 0)
+                    {
+                        break;
+                    }
+                    else if (optionSelected == 1)
+                    {
+                        Console.ResetColor();
+                        Console.Clear();
+                        LoadGame();
+                        break;
+                    }
+                    else if (optionSelected == 2)
+                    {
+                        gameOver = true;
+                        break;
+                    }
+                }
+            }
+            Console.ResetColor();
+            Console.Clear();
+        }
+        static void EndGame() //Displays game over message and ends the game.
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Game Over");
+            Console.ResetColor();
+            Console.ReadKey(true);
+            MainMenu();
+        }
+        static void Main() //Main game loop.
+        {
+            GetFilePath();
+            MainMenu();
+            while (gameOver == false) //gameplay loop, gets page data, parses the page numbers from the data, writes the relevant text to the screen, then waits for player input.
+            {
+                ReadStoryTxt();
+                GetPageNumbers();
+                TextManager();
+                PlayerActions();
+            }
+            EndGame();
+        }
+        static void TitleText()
+        {
             Console.WriteLine("");
             Console.WriteLine("▄█     █▄     ▄█    █▄     ▄█     ▄████████    ▄███████▄    ▄████████    ▄████████    ▄████████");
             Console.WriteLine("███     ███   ███    ███   ███    ███    ███   ███    ███   ███    ███   ███    ███   ███    ███ ");
@@ -131,39 +228,22 @@ namespace InteractiveFiction
             Console.WriteLine("███ ▄█▄ ███   ███    ███   ███     ▄█    ███   ███          ███    ███   ███    ███    ▄█    ███");
             Console.WriteLine(" ▀███▀███▀    ███    █▀    █▀    ▄████████▀   ▄████▀        ██████████   ███    ███  ▄████████▀");
             Console.WriteLine("                                                                         ███    ███             ");
-            Console.WriteLine();
-            Console.WriteLine("Press any key to start.");
-            Console.ReadKey(true);
-            Console.ResetColor();
-            Console.Clear();
-        }
-
-        static void EndGame() //Displays game over message and ends the game.
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Game Over!");
-            Console.ResetColor();
-            Console.ReadKey(true);
-        }
-        static void Main()
-        {
-            MainMenu();
-            while (gameOver == false) //gameplay loop, gets page data, parses the page numbers from the data, writes the relevant text to the screen, then waits for player input.
-            {
-                ReadStoryTxt();
-                GetPageNumbers();
-                //ChangeTextColor();
-                Scribe();
-                PlayerActions();
-            }
-            EndGame();
-        }
-
+            Console.WriteLine("");
+        } //Holds ASCII art for the main menu.
         static void ReadStoryTxt()
         {
-            story = File.ReadLines(@"C:\Users\Kyle\Desktop\GitHub\InteractiveFiction\story.txt").Skip(4).ToArray();
-        }
-
+            string storyTXT = "InteractiveFiction\\story.txt";
+            if (!File.Exists(@useablePath + storyTXT))
+            {
+                Console.WriteLine("story.txt cannot be found @" + useablePath + storyTXT + ". Please ensure story.txt has not been moved or renamed.");
+                Console.ReadKey(true);
+                Environment.Exit(0);
+            }
+            else
+            {
+                story = File.ReadLines(@useablePath + storyTXT).Skip(4).ToArray();
+            }    
+        } //Reads pages from story.txt.
         static void LoadGame()
         {
             Console.WriteLine("Load Game? Any unsaved progress will be lost. Y/N");
@@ -171,7 +251,7 @@ namespace InteractiveFiction
             if (input == ConsoleKey.Y)
             {
 
-                string[] savePoint = File.ReadAllLines(@"C:\Users\Kyle\Desktop\GitHub\InteractiveFiction\savegame.txt").ToArray();
+                string[] savePoint = File.ReadAllLines(@useablePath + "InteractiveFiction\\savegame.txt").ToArray();
                 bool containsInt = int.TryParse(savePoint[1], out int savedPage);
                 if (containsInt == true)
                 {
@@ -195,15 +275,14 @@ namespace InteractiveFiction
                 Console.WriteLine("Invalid input, input must be 'Y' or 'N'");
                 LoadGame();
             } 
-        }
-
+        } //Loads a previous saved game from savegame.txt.
         static void SaveGame()
         {
             Console.WriteLine("Save Game? This will overwrite current save. Y/N");
             ConsoleKey input = Console.ReadKey(true).Key;
             if (input == ConsoleKey.Y)
             {
-                string SavePoint = @"C:\Users\Kyle\Desktop\GitHub\InteractiveFiction\savegame.txt";
+                string SavePoint = @useablePath + "InteractiveFiction\\savegame.txt";
 
                 
                 string[] SaveData = { "Whispers Save Data", Convert.ToString(page) };
@@ -222,6 +301,22 @@ namespace InteractiveFiction
                 Console.WriteLine("Invalid input, input must be 'Y' or 'N'");
                 SaveGame();
             }  
-        }
+        } //Creates or overwrites savegame.txt with save data.
+        static void GetFilePath()
+        {
+            string filePath = AppDomain.CurrentDomain.BaseDirectory;
+            string[] subdirectories = filePath.Split('\\');
+            foreach (string directory in subdirectories)
+            {
+                if (directory.Contains("InteractiveFiction"))
+                {
+                    break;
+                }
+                else
+                {
+                    useablePath = useablePath + directory + "\\";
+                }
+            }
+        } //Gets local file path for SaveGame, LoadGame, and ReadStoryText.
     }
 }
